3bd5e669c86e428ce27372f718cae47aa0ed066e
Glaskani <victor.hachard@hotmail.fr>
2019-05-23 13:41:40 +0200

    Add AddEdit

--- Projet/Projet/EnumGenre.cs
+++ Projet/Projet/EnumGenre.cs
@@ -2,12 +2,48 @@
 {
     public enum EnumGenre
     {
-        Action, 
+        None,
+        Action,
         Drame,
         Comedie,
         Animation,
         Fantastique,
         Horreur,
         Crime
+
+    }
+
+    public class MyConvert
+    {
+        public static EnumGenre stringToGender(string e)
+        {
+            EnumGenre res = EnumGenre.None;
+            switch (e)
+            {
+                case "Action":
+                    res = EnumGenre.Action;
+                    break;
+                case "Drame":
+                    res = EnumGenre.Drame;
+                    break;
+                case "Comedie":
+                    res = EnumGenre.Comedie;
+                    break;
+                case "Animation":
+                    res = EnumGenre.Animation;
+                    break;
+                case "Fantastique":
+                    res = EnumGenre.Fantastique;
+                    break;
+                case "Horreur":
+                    res = EnumGenre.Horreur;
+                    break;
+                case "Crime":
+                    res = EnumGenre.Crime;
+                    break;
+            }
+
+            return res;
+        }
     }
 }
